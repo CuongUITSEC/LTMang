99b2c73bf0e3c8e4e2d396158c2a74ebbf36667e
CuongUITSEC <phammcuongg2005@gmail.com>
2025-04-30 16:25:36 +0700

    trang dang nhap, start

---
+++ TestWPF/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using LoginDemo.ViewModel; // nhớ thêm using namespace của ViewModel
+
+namespace LoginDemo.View
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new MainViewModel();  // Gán ViewModel cho View
+        }
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (this.DataContext is MainViewModel viewModel)
+            {
+                viewModel.Password = (sender as PasswordBox)?.Password;
+            }
+        }
+
+       
+    }
+
+}
---
+++ TestWPF/ViewModel/MainViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+using LoginDemo.Service;
+using LoginDemo.Model;
+
+namespace LoginDemo.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private readonly UserService _userService = new UserService();
+
+        private string _username;
+        private string _password;
+
+        public string Username
+        {
+            get => _username;
+            set
+            {
+                _username = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+
+        public ICommand LoginCommand { get; }
+
+        public MainViewModel()
+        {
+            LoginCommand = new RelayCommand(Login);
+        }
+
+        private void Login()
+        {
+            User user = _userService.GetUserByUsername(Username);
+
+            if (user != null && user.Password == Password)
+            {
+                MessageBox.Show("Login successful!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("Invalid username or password.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
