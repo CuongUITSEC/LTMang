ff7b2149c0e92e127990feb6791c531a911ee53c
CuongUITSEC <phammcuongg2005@gmail.com>
2025-05-02 00:10:11 +0700

    fix bug analyst

--- Learnify/ViewModels/AnalystViewModel.cs
+++ Learnify/ViewModels/AnalystViewModel.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using System;
+using System.Windows.Threading;
 
-namespace Learnify.ViewModels
+public class AnalystViewModel
 {
-    internal class AnalystViewModel
+    public SeriesCollection Series { get; }
+
+    private readonly DispatcherTimer _timer;
+    private int _idx;
+
+    public AnalystViewModel()
     {
+        // Khởi tạo một line series với dữ liệu ban đầu
+        var lineSeries = new LineSeries
+        {
+            Values = new ChartValues<double> { 3, 5, 7, 4 }
+        };
+        Series = new SeriesCollection { lineSeries };
+
+        // Timer cập nhật real-time
+        _timer = new DispatcherTimer
+        {
+            Interval = TimeSpan.FromSeconds(1)
+        };
+        _timer.Tick += (s, e) =>
+        {
+            // Thêm giá trị mới (ví dụ sin wave)
+            double newVal = Math.Sin(_idx++ * 0.2) * 5 + 5;
+            lineSeries.Values.Add(newVal);
+            if (lineSeries.Values.Count > 20)
+                lineSeries.Values.RemoveAt(0);
+        };
+        _timer.Start();
     }
 }
