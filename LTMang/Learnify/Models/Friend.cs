28eb354c21d587f0e453757056e00e2bce60ef60
TranThanhDat23520273 <23520273@gm.uit.edu.vn>
2025-05-16 06:22:36 +0700

    Add files via upload
    nãy chưa save mà đẩy lên :DD

---
+++ LTMang/Learnify/Models/Friend.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learnify.Models
+{
+    public class Friend
+    {
+        public string Name { get; set; }
+        public string Avatar { get; set; } // Đường dẫn tương đối đến ảnh
+        public bool IsOnline { get; set; }
+    }
+}
---
+++ LTMang/Learnify/Views/FOGOT_PW.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Learnify.Views
+{
+    /// <summary>
+    /// Interaction logic for FOGOT_PW.xaml
+    /// </summary>
+    public partial class FOGOT_PW : Window
+    {
+        public FOGOT_PW()
+        {
+            InitializeComponent();
+        }
+    }
+}
